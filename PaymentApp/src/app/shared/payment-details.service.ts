4cdbcdf58fd022e0f001c9f6534edbc10841675b
Awab M.Najim <44520076+Gandol-SD@users.noreply.github.com>
2021-05-29 09:50:02 +0200

    Add form elements

--- PaymentApp/src/app/shared/payment-details.service.ts
+++ PaymentApp/src/app/shared/payment-details.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { PaymentDetails } from './payment-details.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentDetailsService { 
-
+  formData:PaymentDetails = new PaymentDetails();
+  
   constructor() { }
 }
